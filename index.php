f966a0cc661e1cb92791e3337e0e37d66e4883e2
arturo <arturomazzoni@gmail.com>
2021-02-24 17:46:51 +0100

    commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$nome = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+$nome = str_replace("ipsum", "***", $nome);
+echo $nome;
+$lunghezza = strlen($nome);
+echo "<br>";
+echo "Lunghezza stringa: " .$lunghezza;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
